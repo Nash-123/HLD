30381f47bf8dc6cf5bdaedd4676ecd7c97b76c39
Nishan Acharya <mamabapuni@gmail.com>
2025-12-19 02:08:43 +0530

    Add CPU Intensive Task

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 
 app.get("/ping", (req,res) => {
+    for(int i=0;i<100000000000;i++){}
     return res.json({
         message: "Ping Suuccessful",
         ip: ip.address()
