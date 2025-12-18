37d85726324d91c04d5eb813c3023764a71b78bf
Nishan Acharya <mamabapuni@gmail.com>
2025-12-19 03:03:06 +0530

    Add API for heavy tasks

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const app = express();
 
 
 app.get("/ping", (req,res) => {
-    for(int i=0;i<100000000000;i++){}
     return res.json({
         message: "Ping Suuccessful",
         ip: ip.address()
     })
+});
+
+app.get("/heavy", (req,res) => {
+
+    for(let i=0;i<1000000;i++){};
+    return res.json({
+        message: "Heavy Endpoint Processed",
+        ip: ip.address()
+    });
 });
 
 app.listen(3000, () => {
